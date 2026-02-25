376adb92c214680bcb44a58d3edc5151f4d094f6
findmehul <97856695+findmehul@users.noreply.github.com>
2026-02-25 12:27:52 -0500

    remove aws_lb_target_group_attachment and fix security group reference.

--- main.tf
+++ main.tf
@@ -76,12 +76,6 @@ resource "aws_lb_target_group" "blog" {
   vpc_id   = module.blog_vpc.vpc_id
 }
 
-resource "aws_lb_target_group_attachment" "blog" {
-  target_group_arn = aws_lb_target_group.blog.arn
-  target_id        = aws_instance.blog.id
-  port             = 80
-}
-
 module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "9.2.0"
@@ -94,7 +88,7 @@ module "blog_autoscaling" {
   vpc_zone_identifier = module.blog_vpc.private_subnets
 
   launch_template_name   = "blog"
-  security_groups        = [module.blog.security_group_id]
+  security_groups        = [module.blog_sg.security_group_id]
   instance_type          = var.instance_type
   image_id               = data.aws_ami.app_ami.id
 
